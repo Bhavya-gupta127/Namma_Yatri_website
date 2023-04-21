388730042ecfa7fe3c7d6280dce88487cc92d9ef
Bhavya-gupta127 <20ucs052@lnmiit.ac.in>
2023-04-21 21:46:07 +0530

    map added

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,16 @@ import "@/styles/globals.css";
 // import Name from "@/pages/name";
 // import Otp from "@/pages/otp";
 import Navbar from "./navbar";
+import Map from "../components/map"
+import "mapbox-gl/dist/mapbox-gl.css"
+import Login from "./login";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
+      <Map/>
+      {/* <Login/>  */}
       <Component {...pageProps} />
     </>
   );
