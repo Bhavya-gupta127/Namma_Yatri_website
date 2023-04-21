03ce60a59a9ea64281c15bed705ca829653a8293
Bhavya-gupta127 <20ucs052@lnmiit.ac.in>
2023-04-22 04:31:46 +0530

    markers added to map

--- components/map.js
+++ components/map.js
@@ -2,21 +2,85 @@ import { useEffect, useState } from "react"
 
 import "mapbox-gl/dist/mapbox-gl.css"
 import mapboxgl from "mapbox-gl";
+// const MapboxDirections = require('mapbox-gl/dist/mapbox-gl.js')
+// import Mapa from '@/components/modules/Home/Map/Map'
+
+// import MapboxDirections from '@mapbox/mapbox-gl-directions';
+
+// import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+// import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+
+import useStore from "@/lib/store";
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmhhdnlhZ3VwdGExMjciLCJhIjoiY2xncGY3Mml3MHJ5MzNkcDkya2JoZWxxaCJ9.8UoSDJE-QV7fWvj3pMcwcw';
 
 const Map = () => {
 const [lng, setLng] = useState(-70.9);
 const [lat, setLat] = useState(42.35);
-const [zoom, setZoom] = useState(9);
-  useEffect(() => {
-      if (map.current) return; // initialize map only once
-      map.current = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom
-      });
+const [zoom, setZoom] = useState(3);
+const [pickLocation, setPickLocation] = useState([1,1]);
+const [dropLocation, setDropLocation] = useState([1,1]);
+const pick=useStore((state) => state.pickLocation);
+const drop=useStore((state) => state.dropLocation);
+useEffect(() => {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [lng, lat],
+    zoom: zoom
+  });
+  // map.addControl(
+  //   new MapboxDirections({
+  //   accessToken: mapboxgl.accessToken
+  //   }),
+  //   'top-left'
+  //   );
+
+  map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+    enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserHeading: true
+    }));
+    const projection = map.getProjection();
+
+  const nav = new mapboxgl.NavigationControl({
+    visualizePitch: true
     });
+    map.addControl(nav, 'bottom-right');
+  // const geolocate = new mapboxgl.GeolocateControl({
+  //   positionOptions: {
+  //   enableHighAccuracy: true
+  //   },
+  //   trackUserLocation: true
+  //   });
+  // map.addControl(geolocate);
+  // // Set an event listener that fires
+  // // when a trackuserlocationstart event occurs.
+  // geolocate.on('trackuserlocationstart', () => {
+  // console.log('A trackuserlocationstart event has occurred.');
+  // }); 
+      setPickLocation(pick);
+      setDropLocation(drop);
+      if(pickLocation)
+        addToMap(map,pickLocation); 
+      if(dropLocation)
+        addToMap(map,dropLocation);
+      if(pickLocation && dropLocation)
+      {
+        map.fitBounds([
+            dropLocation, // southwestern corner of the bounds
+            pickLocation // northeastern corner of the bounds
+          ]);
+      }
+
+    },[pickLocation,dropLocation]);
+
+    const addToMap=(map,coordinates)=>{
+      const marker1 = new mapboxgl.Marker()
+      .setLngLat(coordinates)
+      .addTo(map);
+    }
     
   return (
     <div id='map'> map</div>
--- pages/name.js
+++ pages/name.js
@@ -1,12 +1,14 @@
 import useStore from "@/lib/store";
-import { useState } from "react";
+import { data } from "autoprefixer";
+import { useEffect, useState } from "react";
 
 //ADD CODE TO VALIDATE THE NAME
 const Login = () => {
   const [name, setName] = useState("");
-  // const name1 = useStore((state) => state.name);
+  const name1 = useStore((state) => state.name);
   const setName1 = useStore((state) => state.setName);
-
+  const setPickLocation = useStore((state) => state.setPickLocation);
+  const setDropLocation = useStore((state) => state.setDropLocation);
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
@@ -21,6 +23,40 @@ const Login = () => {
     setName(event.target.value);
   }
 
+  // add this function to booking page
+  const getPickCoordinates=()=>{
+      const pickLocation="ambala" // make it dynamic
+      fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickLocation}.json?`+
+      new URLSearchParams({
+        access_token: "pk.eyJ1IjoiYmhhdnlhZ3VwdGExMjciLCJhIjoiY2xncGY3Mml3MHJ5MzNkcDkya2JoZWxxaCJ9.8UoSDJE-QV7fWvj3pMcwcw",
+        limit:1
+      })
+      )
+      .then(response=> response.json())
+      .then(data=>{
+          console.log(data.features[0].center);
+          setPickLocation(data.features[0].center);
+      })
+  }
+  const getDropCoordinates=()=>{
+      const dropLocation="delhi" // make it dynamic
+      fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropLocation}.json?`+
+      new URLSearchParams({
+        access_token: "pk.eyJ1IjoiYmhhdnlhZ3VwdGExMjciLCJhIjoiY2xncGY3Mml3MHJ5MzNkcDkya2JoZWxxaCJ9.8UoSDJE-QV7fWvj3pMcwcw",
+        limit:1
+      })
+      )
+      .then(response=> response.json())
+      .then(data=>{
+          console.log(data.features[0].center);
+          setDropLocation(data.features[0].center);
+      })
+  }
+  useEffect(() => {
+    getPickCoordinates();
+    getDropCoordinates();
+  }, [])
+  
   return (
     <div className=" text-4xl  h-screen flex temp">
       <div className=" mh-full  m-auto flex-col items-center bg-white rounded-lg sm:border sm:border-primaryBorder shadow-default py-40 px-16">
