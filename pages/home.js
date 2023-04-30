1448d678bdb59af31913e9683fb42252c13b9a50
Bhavya-gupta127 <20ucs052@lnmiit.ac.in>
2023-04-30 15:57:17 +0530

    map search implemented

--- pages/home.js
+++ pages/home.js
@@ -15,50 +15,7 @@ const Home = () => {
 
   return (
 
-    <div className=" text-4xl  h-screen flex  temp">
-      <div className=" mh-full  m-auto flex-col items-center bg-white  rounded-lg sm:border sm:border-primaryBorder shadow-default py-40 px-16">
-        <h1 className="text-blackfont-medium text-primary mt-4 mb-12 text-center">
-          Choose Your Location
-        </h1>
-
-        <form>
-          <div className="w-screen max-w-xs">
-            {/* <label htmlFor="number">Enter your Mobile Number</label> */}
-            {/* <br /> */}
-            <input
-              type="text"
-              name="from"
-              className={` w-full p-2 text-primary border rounded-md outline-none text-xl transition duration-150 ease-in-out mb-4`}
-              id="from"
-              placeholder="From"
-              onChange={handleChange}
-              value={from}
-              autoComplete="off"
-            />
-            <input
-              type="text"
-              name="to"
-              className={` w-full p-2 text-primary border rounded-md outline-none text-xl transition duration-150 ease-in-out mb-4`}
-              id="to"
-              placeholder="To"
-              onChange={handleChange}
-              value={to}
-              autoComplete="off"
-            />
-          </div>
-          <br />
-          <div className="flex justify-center items-center mt-6">
-            <Link href={{ pathname: "/request" }}>
-              <button
-                className={`w-full bg-black text-white font-medium bg-green py-2 px-4 text-xl rounded border border-green focus:outline-none focus:border-green-dark`}
-              >
-                Continue
-              </button>
-            </Link>
-          </div>
-        </form>
-      </div>
-    </div>
+   null
 
   );
 };
